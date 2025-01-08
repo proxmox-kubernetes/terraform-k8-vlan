91ff8a61c84a86be4dbfc046f39e681a13cb21fc
Reggles <reginaldbeakes@gmail.com>
2025-01-07 23:07:49 -0500

    updates for terragrunt

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       version = "0.11.0"
       source  = "browningluke/opnsense"
     }
-    macaddress = {
-      version = "0.3.2"
-      source  = "ivoronin/macaddress"
-    }
   }
 }
 
@@ -18,18 +14,10 @@ provider "opnsense" {
   allow_insecure = true
 }
 
-resource "opnsense_interfaces_vlan" "k8_vlan" {
-  device = "K8"
-  description = "K8 VLAN"
-  tag = 8
-  priority = 0
-  parent = "vtnet0"
-}
-
 resource "opnsense_kea_subnet" "k8_subnet" {
-  subnet = var.subnet_ip
+  subnet      = var.subnet_ip
   description = var.subnet_desc
-  
+
   pools = [
     var.subnet_pool
   ]
